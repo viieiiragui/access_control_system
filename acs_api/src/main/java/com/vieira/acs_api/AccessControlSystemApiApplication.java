951b1ed2c3d82fb0282e84e76281a3ce877b0edd
Guilherme Vieira <guii_vieira@outlook.com.br>
2025-07-19 15:09:34 -0300

    chore: init Access Control System API

---
+++ acs_api/src/main/java/com/vieira/acs_api/AccessControlSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.vieira.acs_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlSystemApiApplication.class, args);
+	}
+
+}
---
+++ acs_api/src/test/java/com/vieira/acs_api/AccessControlSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieira.acs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
